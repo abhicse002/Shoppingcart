05b2812c17169dc5fa46a1cdcc2a87578bd27870
abhilashs <abhicse002@gmail.com>
2021-01-19 20:18:06 +0530

    abstract class added to Products model

--- Products/models.py
+++ Products/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-# Create your models here.
-class Products(models.Model):
-    product_name = models.CharField(max_length=100)
-    product_desc =models.CharField(max_length=100)
-    product_id = models.CharField(max_length=100)
 
+# Create your models here.
+
+class AbstractclassforProduct(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
+    upload = models.ImageField()
+
+
+class Products(AbstractclassforProduct):
+    product_id = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Categories(AbstractclassforProduct):
+    category_id = models.CharField(max_length=100)
